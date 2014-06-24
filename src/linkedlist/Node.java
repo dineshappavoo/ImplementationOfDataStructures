1cbb076c00fd3f4d3661669c02e066799072b89d
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-06-23 23:42:20 -0500

    LinkedList Node impl in java

---
+++ src/linkedlist/Node.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package linkedlist;
+
+/**
+ * @author Dany
+ *
+ */
+public class Node {
+
+	
+	public int data;
+	public Node next;
+	public Node(int data)
+	{
+		this.data=data;
+	}
+	public Node()
+	{
+		
+	}
+}
