083e35223fa5b81aaae3c5caddf71bbb6688b9f3
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-06-23 23:41:56 -0500

    LinkedList test impl in java

---
+++ src/linkedlist/LinkedListTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package linkedlist;
+
+/**
+ * @author Dany
+ *
+ */
+public class LinkedListTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		
+		LinkedList lList=new LinkedList();
+		lList.insert(4);
+		lList.insert(10);
+		lList.insert(15);
+		lList.insert(54);
+		
+		lList.traverseList();
+		
+	}
+	
+	
+
+}
